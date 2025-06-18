eec6a600d4ba1a51f51c9703adf504b442c456db
Daniel Morales <Daniem976@gmail.com>
2025-06-18 17:53:46 +0000

    added name inputs

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-document.getElementById("greeting").textContent = "hey there monke";
-document.getElementById("header").textContent = "Da title";
+addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('button');
+
+    btn.addEventListener('click', () => {
+      let name = document.getElementById("name").value;
+      console.log('Button clicked!');
+      document.getElementById("header").textContent ="Welcome " + name ;
+    });
+    
+    if (name.value = "monke")() => {
+      document.body.style.backgroundColor("brown");
+    }
+
+});
